6fff30a7541e35330805ed0fa7ffd8629a5581f9
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-03-04 14:06:37 +0300

    change a to NavLink

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import classes from "./Sidebar.module.css";
 function Sidebar() {
   return (
     <aside className={classes.sidebar}>
       <ul className={classes.list}>
-        <li className={classes.item}><a href="/profile" className={`${classes.link} ${classes.active}`}>Profile</a></li>
-        <li className={classes.item}><a href="/dialogs" className={classes.link}>Message</a></li>
-        <li className={classes.item}><a href="/news" className={classes.link}>News</a></li>
-        <li className={classes.item}><a href="/music" className={classes.link}>Music</a></li>
-        <li className={classes.item}><a href="/settings" className={classes.link}>Settings</a></li>
+        <li className={classes.item}><NavLink to="/profile" className={`${classes.link} ${classes.active}`}>Profile</NavLink></li>
+        <li className={classes.item}><NavLink to="/dialogs" className={classes.link}>Message</NavLink></li>
+        <li className={classes.item}><NavLink to="/news" className={classes.link}>News</NavLink></li>
+        <li className={classes.item}><NavLink to="/music" className={classes.link}>Music</NavLink></li>
+        <li className={classes.item}><NavLink to="/settings" className={classes.link}>Settings</NavLink></li>
       </ul>
     </aside>
   )
