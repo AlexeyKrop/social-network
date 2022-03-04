2595a5efff16478b0bf6c5ac9d3a70a2950ca0ee
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-03-04 14:57:22 +0300

    add style for dialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -2,7 +2,18 @@ import React from 'react';
 import classes from './Dialogs.module.css';
 const Dialogs = () =>{
   return (
-    <div>hello</div>
+    <div className={classes.wrapper}>
+      <ul className={classes.user}>
+        <li className={classes.user__item}>Tiger</li>
+        <li className={classes.user__item}>Leo</li>
+        <li className={classes.user__item}>Monkey</li>
+      </ul>
+      <ul className={classes.dialogs}>
+        <li className={classes.dialogs}>Hello</li>
+        <li className={classes.dialogs}>How are you</li>
+        <li className={classes.dialogs}>Good</li>
+      </ul>
+    </div>
   )
 }
 export default Dialogs
