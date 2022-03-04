13f7761d4d2f98b501c01522c4b9984749332ac8
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-03-04 13:10:19 +0300

    install router dom and add new file

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,32 @@ import './App.css';
 import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Profile from "./components/Profile/Profile";
+import Dialogs from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
 
 
 function App() {
   return (
-    <div className="App">
-      <div className="container">
-        <Header />
-        <div className="wrapper">
-          <Sidebar />
-          <main className="content">
-            <Profile />
-          </main>
+    <BrowserRouter>
+      <div className="App">
+        <div className="container">
+          <Header />
+          <div className="wrapper">
+            <Sidebar />
+            <main className="content">
+              <Route path="/profile" component={Profile} />
+              <Route path="/dialogs" component={Dialogs} />
+              <Route path="/news" component={News} />
+              <Route path="/music" component={Music} />
+              <Route path="/settings" component={Settings} />
+            </main>
+          </div>
         </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -4,11 +4,11 @@ function Sidebar() {
   return (
     <aside className={classes.sidebar}>
       <ul className={classes.list}>
-        <li className={classes.item}><a href="/" className={classes.link}>Profile</a></li>
-        <li className={classes.item}><a href="/" className={classes.link}>Message</a></li>
-        <li className={classes.item}><a href="/" className={classes.link}>News</a></li>
-        <li className={classes.item}><a href="/" className={classes.link}>Music</a></li>
-        <li className={classes.item}><a href="/" className={classes.link}>Settings</a></li>
+        <li className={classes.item}><a href="/profile" className={`${classes.link} ${classes.active}`}>Profile</a></li>
+        <li className={classes.item}><a href="/dialogs" className={classes.link}>Message</a></li>
+        <li className={classes.item}><a href="/news" className={classes.link}>News</a></li>
+        <li className={classes.item}><a href="/music" className={classes.link}>Music</a></li>
+        <li className={classes.item}><a href="/settings" className={classes.link}>Settings</a></li>
       </ul>
     </aside>
   )
