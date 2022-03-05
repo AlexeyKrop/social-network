f5dbedb09410bd91e6823ce5c671ace39d5d7ecd
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-03-05 13:17:22 +0300

    add user-block add icon for user-block

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import logo from "../../logo.png";
 import classes from "./Header.module.css";
+import avatar from '../../ava.jpg'
 const Header = () => {
   return (
     <header className={classes.header}>
@@ -33,16 +34,35 @@ const Header = () => {
                 />
               </svg>
             </NavLink></li>
-            <li className={classes.item}><NavLink to="/music" className={classes.link}><svg className={classes.icon} xmlns="http://www.w3.org/2000/svg" id="Outline" viewBox="0 0 24 24" width="20"
+            <li className={classes.item}><NavLink to="/music" className={classes.link}>
+              <svg className={classes.icon} xmlns="http://www.w3.org/2000/svg" id="Outline" viewBox="0 0 24 24"
+                   width="20"
                    height="20" fill='white'>
                 <path
                   d="M19,1H13A5.006,5.006,0,0,0,8,6v8.026A4.948,4.948,0,0,0,5,13a5,5,0,1,0,5,5V6a3,3,0,0,1,3-3h6a3,3,0,0,1,3,3v8.026A4.948,4.948,0,0,0,19,13a5,5,0,1,0,5,5V6A5.006,5.006,0,0,0,19,1ZM5,21a3,3,0,1,1,3-3A3,3,0,0,1,5,21Zm14,0a3,3,0,1,1,3-3A3,3,0,0,1,19,21Z"/>
-              </svg><sup className={classes.num}>1</sup>
+              </svg>
+              <sup className={classes.num}>1</sup>
             </NavLink></li>
           </ul>
           <ul className={classes.list}>
-            <li className={classes.item}><a href="/" className={classes.link}>ava</a></li>
-            <li className={classes.item}><a href="/" className={classes.link}>login</a><a href="/" className={classes.link}>password</a></li>
+            <li className={`${classes.item} ${classes.item_user}`}><a href="/" className={classes.link}><img
+              className={classes.image}
+              src={avatar}
+              alt="аватар"/>
+            </a>
+              <span className={classes.user_name}>Smith</span>
+            </li>
+            <li className={classes.item}><NavLink to="/" className={classes.link}>
+              <svg className={classes.icon} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20"
+                   height="20"
+                   fill='#fff'>
+                <g id="_01_align_center" data-name="01 align center">
+                  <path
+                    d="M15,24H9V20.487a9,9,0,0,1-2.849-1.646L3.107,20.6l-3-5.2L3.15,13.645a9.1,9.1,0,0,1,0-3.29L.107,8.6l3-5.2L6.151,5.159A9,9,0,0,1,9,3.513V0h6V3.513a9,9,0,0,1,2.849,1.646L20.893,3.4l3,5.2L20.85,10.355a9.1,9.1,0,0,1,0,3.29L23.893,15.4l-3,5.2-3.044-1.758A9,9,0,0,1,15,20.487Zm-4-2h2V18.973l.751-.194A6.984,6.984,0,0,0,16.994,16.9l.543-.553,2.623,1.515,1-1.732-2.62-1.513.206-.746a7.048,7.048,0,0,0,0-3.75l-.206-.746,2.62-1.513-1-1.732L17.537,7.649,16.994,7.1a6.984,6.984,0,0,0-3.243-1.875L13,5.027V2H11V5.027l-.751.194A6.984,6.984,0,0,0,7.006,7.1l-.543.553L3.84,6.134l-1,1.732L5.46,9.379l-.206.746a7.048,7.048,0,0,0,0,3.75l.206.746L2.84,16.134l1,1.732,2.623-1.515.543.553a6.984,6.984,0,0,0,3.243,1.875l.751.194Zm1-6a4,4,0,1,1,4-4A4,4,0,0,1,12,16Zm0-6a2,2,0,1,0,2,2A2,2,0,0,0,12,10Z"/>
+                </g>
+              </svg>
+            </NavLink>
+            </li>
           </ul>
         </nav>
       </div>
