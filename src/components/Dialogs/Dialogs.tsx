c10b3b26fe32763d4bd51a78cc3e81e893f9f01d
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-03-09 12:58:28 +0300

    fix dialogs for component

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 import classes from './Dialogs.module.css';
+
 type UserItemPropsType = {
   src: string;
   name: string;
   id: string;
 }
-const UserDialogsItem = (props: UserItemPropsType) =>{
-  return(
+type MessagePropsType = {
+  src: string,
+  message: string,
+  message_time: string,
+  classForChat: string,
+}
+const UserDialogsItem = (props: UserItemPropsType) => {
+  return (
     <li className={classes.user__item}><NavLink to={"/dialogs/" + props.id}>
       <img className={classes.user__avatar}
            src={props.src}
@@ -15,75 +22,51 @@ const UserDialogsItem = (props: UserItemPropsType) =>{
     </NavLink>{props.name}</li>
   )
 }
+const MessageDialogsItem = (props: MessagePropsType) => {
+  return (
+    <div className={`${classes.chat} ${props.classForChat}`}>
+      <div className={classes.chat__avatar}>
+        <a href="/">
+          <img
+            src={props.src}
+            alt="img"/>
+        </a>
+      </div>
+      <div className={classes.chat__body}>
+        <div className={classes.chat__message}>
+          <p>{props.message}</p>
+          <span>{props.message_time}</span>
+        </div>
+      </div>
+    </div>
+  )
+}
 const Dialogs = () => {
   return (
     <div className={classes.wrapper}>
       <ul className={classes.user}>
-        <UserDialogsItem src="https://avatars.mds.yandex.net/get-kino-vod-films-gallery/28788/47e2fd514411e18b76af786d7417062d/100x64_3" name="Kelly Smith" id='1'/>
-        <UserDialogsItem src="https://static.1tv.ru/uploads/photo/image/2/huge/4062_huge_876c41f50e.jpg" name="Stepan Bogdan" id='2'/>
+        <UserDialogsItem
+          src="https://avatars.mds.yandex.net/get-kino-vod-films-gallery/28788/47e2fd514411e18b76af786d7417062d/100x64_3"
+          name="Kelly Smith" id='1'/>
+        <UserDialogsItem src="https://static.1tv.ru/uploads/photo/image/2/huge/4062_huge_876c41f50e.jpg"
+                         name="Stepan Bogdan" id='2'/>
         <UserDialogsItem src="https://cdnimg.rg.ru/i/gallery/84f24d10/19_b6265e7a.jpg" name="Alex Piter" id='3'/>
-        <UserDialogsItem src="https://pluggedin.ru/images/1-bigTopImage_2021_08_17_20_35_36.jpg" name="James Smith" id='4'/>
+        <UserDialogsItem src="https://pluggedin.ru/images/1-bigTopImage_2021_08_17_20_35_36.jpg" name="James Smith"
+                         id='4'/>
       </ul>
-      <div className={classes.chat}>
-        <div className={classes.chat__avatar}>
-          <a href="/" className="d-inline-block">
-            <img
-              src="https://avatars.mds.yandex.net/get-kino-vod-films-gallery/28788/47e2fd514411e18b76af786d7417062d/100x64_3"
-              className="rounded-circle" alt="img"/>
-          </a>
-        </div>
-        <div className={classes.chat__body}>
-          <div className={classes.chat__message}>
-            <p>Hello, dear I want talk to you?</p>
-            <span className="time d-block">7:45 AM</span>
-          </div>
-        </div>
-      </div>
-      <div className={`${classes.chat} ${classes.chat__left}`}>
-        <div className={classes.chat__avatar}>
-          <a href="/" className="d-inline-block">
-            <img src="https://static.1tv.ru/uploads/photo/image/2/huge/4062_huge_876c41f50e.jpg"
-                 className="rounded-circle" alt="img"/>
-          </a>
-        </div>
-
-        <div className={classes.chat__body}>
-          <div className={classes.chat__message}>
-            <p>Said how can I cooperate with you?</p>
-            <span className="time d-block">7:45 AM</span>
-          </div>
-        </div>
-      </div>
-      <div className={classes.chat}>
-        <div className={classes.chat__avatar}>
-          <a href="/" className="d-inline-block">
-            <img
-              src="https://cdnimg.rg.ru/i/gallery/84f24d10/19_b6265e7a.jpg"
-              alt="img"/>
-          </a>
-        </div>
-        <div className={classes.chat__body}>
-          <div className={classes.chat__message}>
-            <p>What you want to know about design</p>
-            <span>7:45 AM</span>
-          </div>
-        </div>
-      </div>
-      <div className={`${classes.chat} ${classes.chat__left}`}>
-        <div className={classes.chat__avatar}>
-          <a href="/" className="d-inline-block">
-            <img src="https://pluggedin.ru/images/1-bigTopImage_2021_08_17_20_35_36.jpg"
-                 alt="img"/>
-          </a>
-        </div>
-
-        <div className={classes.chat__body}>
-          <div className={classes.chat__message}>
-            <p>I need some ideas from you about my design</p>
-            <span>7:45 AM</span>
-          </div>
-        </div>
-      </div>
+      <MessageDialogsItem
+        src="https://avatars.mds.yandex.net/get-kino-vod-films-gallery/28788/47e2fd514411e18b76af786d7417062d/100x64_3"
+        message="Hello, dear I want talk to you?"
+        message_time="7 45 АМ"
+        classForChat=""/>
+      <MessageDialogsItem src="https://static.1tv.ru/uploads/photo/image/2/huge/4062_huge_876c41f50e.jpg"
+                          message="Said how can I cooperate with you?" message_time="8 45 АМ"
+                          classForChat={classes.chat__left}/>
+      <MessageDialogsItem src="https://cdnimg.rg.ru/i/gallery/84f24d10/19_b6265e7a.jpg"
+                          message="I need some ideas from you about my design" message_time="9 45 АМ" classForChat=""/>
+      <MessageDialogsItem src="https://pluggedin.ru/images/1-bigTopImage_2021_08_17_20_35_36.jpg"
+                          message="I need some ideas from you about my design" message_time="10 45 АМ"
+                          classForChat={classes.chat__left}/>
     </div>
   )
 }
