b1e0b689782d2675b2a957e684c71f0c0ac37571
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-03-09 10:37:47 +0300

    fix image friend from header component

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -6,9 +6,9 @@ type UserItemPropsType = {
   name: string;
   id: string;
 }
-const UserItem = (props: UserItemPropsType) =>{
+const UserDialogsItem = (props: UserItemPropsType) =>{
   return(
-    <li className={classes.user__item}><NavLink to={"/dialogs" + props.id}>
+    <li className={classes.user__item}><NavLink to={"/dialogs/" + props.id}>
       <img className={classes.user__avatar}
            src={props.src}
            alt="img"/>
@@ -19,10 +19,10 @@ const Dialogs = () => {
   return (
     <div className={classes.wrapper}>
       <ul className={classes.user}>
-        <UserItem src="https://avatars.mds.yandex.net/get-kino-vod-films-gallery/28788/47e2fd514411e18b76af786d7417062d/100x64_3" name="Kelly Smith" id='1'/>
-        <UserItem src="https://static.1tv.ru/uploads/photo/image/2/huge/4062_huge_876c41f50e.jpg" name="Stepan Bogdan" id='2'/>
-        <UserItem src="https://cdnimg.rg.ru/i/gallery/84f24d10/19_b6265e7a.jpg" name="Alex Piter" id='3'/>
-        <UserItem src="https://pluggedin.ru/images/1-bigTopImage_2021_08_17_20_35_36.jpg" name="James Smith" id='4'/>
+        <UserDialogsItem src="https://avatars.mds.yandex.net/get-kino-vod-films-gallery/28788/47e2fd514411e18b76af786d7417062d/100x64_3" name="Kelly Smith" id='1'/>
+        <UserDialogsItem src="https://static.1tv.ru/uploads/photo/image/2/huge/4062_huge_876c41f50e.jpg" name="Stepan Bogdan" id='2'/>
+        <UserDialogsItem src="https://cdnimg.rg.ru/i/gallery/84f24d10/19_b6265e7a.jpg" name="Alex Piter" id='3'/>
+        <UserDialogsItem src="https://pluggedin.ru/images/1-bigTopImage_2021_08_17_20_35_36.jpg" name="James Smith" id='4'/>
       </ul>
       <div className={classes.chat}>
         <div className={classes.chat__avatar}>
