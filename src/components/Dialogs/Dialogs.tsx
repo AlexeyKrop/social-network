53df0bb54de20c1b690bc515725373c5b8d4a369
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-03-09 15:59:30 +0300

    add ava + descriptions and add style from this component

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -11,7 +11,7 @@ type MessagePropsType = {
   src: string,
   message: string,
   message_time: string,
-  classForChat: string,
+  className: string,
 }
 const UserDialogsItem = (props: UserItemPropsType) => {
   return (
@@ -24,7 +24,7 @@ const UserDialogsItem = (props: UserItemPropsType) => {
 }
 const MessageDialogsItem = (props: MessagePropsType) => {
   return (
-    <div className={`${classes.chat} ${props.classForChat}`}>
+    <div className={`${classes.chat} ${props.className}`}>
       <div className={classes.chat__avatar}>
         <a href="/">
           <img
@@ -58,15 +58,15 @@ const Dialogs = () => {
         src="https://avatars.mds.yandex.net/get-kino-vod-films-gallery/28788/47e2fd514411e18b76af786d7417062d/100x64_3"
         message="Hello, dear I want talk to you?"
         message_time="7 45 АМ"
-        classForChat=""/>
+        className=""/>
       <MessageDialogsItem src="https://static.1tv.ru/uploads/photo/image/2/huge/4062_huge_876c41f50e.jpg"
                           message="Said how can I cooperate with you?" message_time="8 45 АМ"
-                          classForChat={classes.chat__left}/>
+                          className={classes.chat__left}/>
       <MessageDialogsItem src="https://cdnimg.rg.ru/i/gallery/84f24d10/19_b6265e7a.jpg"
-                          message="I need some ideas from you about my design" message_time="9 45 АМ" classForChat=""/>
+                          message="I need some ideas from you about my design" message_time="9 45 АМ" className=""/>
       <MessageDialogsItem src="https://pluggedin.ru/images/1-bigTopImage_2021_08_17_20_35_36.jpg"
                           message="I need some ideas from you about my design" message_time="10 45 АМ"
-                          classForChat={classes.chat__left}/>
+                          className={classes.chat__left}/>
     </div>
   )
 }
