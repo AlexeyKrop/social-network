63423f33b0b81caa6c3bddc98ddc14d3e080b8ae
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-03-12 17:55:38 +0300

    add style from friends component

--- src/components/Friends/Friends.tsx
+++ src/components/Friends/Friends.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
 import classes from './Friends.module.css'
+import card_image_bg from '../../friends_bg_1.jpg'
+import card_image from '../../friends_1.jpg'
 
 const Friends = () => {
   return (
     <div className={classes.list}>
-      <div className="col-lg-3 col-sm-6">
+      <div className={classes.row}>
         <div className={classes.card}>
-          <div className="friends-image">
+          <div className={classes.friends__image}>
             <NavLink to="#">
-              {/*<img src="assets/images/friends/friends-bg-1.jpg" alt="image"/>*/}
+              <img src={card_image_bg} alt="img_bg"/>
             </NavLink>
-            <div className="icon">
-              <NavLink to="#">
-                <svg className={classes.icon} height="20" viewBox="0 0 24 24" width="20"
+            <div className={classes.icon}>
+              <NavLink className={classes.link} to="#">
+                <svg height="20" viewBox="0 0 24 24" width="20"
                      xmlns="http://www.w3.org/2000/svg"
                      fill='grey'>
                   <path
@@ -22,41 +24,321 @@ const Friends = () => {
               </NavLink>
             </div>
           </div>
-          <div className="friends-content">
-            <div className="friends-info d-flex justify-content-between align-items-center">
+          <div className={classes.friends__content}>
+            <div className={classes.friends__info}>
               <NavLink to="#">
-                {/*<img src="assets/images/friends/friends-1.jpg" alt="image"/>*/}
+                <img src={card_image} alt="img_card"/>
               </NavLink>
-              <div className="text ms-3">
+              <div className={classes.text}>
                 <h3><NavLink to="#">Jose Marroquin</NavLink></h3>
                 <span>10 Mutual Friends</span>
               </div>
             </div>
-            <ul className="statistics">
+            <ul className={classes.statistics}>
               <li>
                 <NavLink to="#">
-                  <span className="item-number">862</span>
-                  <span className="item-text">Likes</span>
+                  <span className={classes.item_number}>862</span>
+                  <span className={classes.item_text}>Likes</span>
                 </NavLink>
               </li>
               <li>
                 <NavLink to="#">
-                  <span className="item-number">91</span>
-                  <span className="item-text">Following</span>
+                  <span className={classes.item_number}>91</span>
+                  <span className={classes.item_text}>Following</span>
                 </NavLink>
               </li>
               <li>
                 <NavLink to="#">
-                  <span className="item-number">514</span>
-                  <span className="item-text">Followers</span>
+                  <span className={classes.item_number}>514</span>
+                  <span className={classes.item_text}>Followers</span>
                 </NavLink>
               </li>
             </ul>
-            <div className="button-group d-flex justify-content-between align-items-center">
-              <div className="add-friend-btn">
+            <div className={classes.button_group}>
+              <div className={classes.add_friend_btn}>
                 <button type="submit">Add Friend</button>
               </div>
-              <div className="send-message-btn">
+              <div className={classes.send_message_btn}>
+                <button type="submit">Send Message</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={classes.card}>
+          <div className={classes.friends__image}>
+            <NavLink to="#">
+              <img src={card_image_bg} alt="img_bg"/>
+            </NavLink>
+            <div className={classes.icon}>
+              <NavLink className={classes.link} to="#">
+                <svg height="20" viewBox="0 0 24 24" width="20"
+                     xmlns="http://www.w3.org/2000/svg"
+                     fill='grey'>
+                  <path
+                    d="m7.5 13a4.5 4.5 0 1 1 4.5-4.5 4.505 4.505 0 0 1 -4.5 4.5zm0-7a2.5 2.5 0 1 0 2.5 2.5 2.5 2.5 0 0 0 -2.5-2.5zm7.5 17v-.5a7.5 7.5 0 0 0 -15 0v.5a1 1 0 0 0 2 0v-.5a5.5 5.5 0 0 1 11 0v.5a1 1 0 0 0 2 0zm9-5a7 7 0 0 0 -11.667-5.217 1 1 0 1 0 1.334 1.49 5 5 0 0 1 8.333 3.727 1 1 0 0 0 2 0zm-6.5-9a4.5 4.5 0 1 1 4.5-4.5 4.505 4.505 0 0 1 -4.5 4.5zm0-7a2.5 2.5 0 1 0 2.5 2.5 2.5 2.5 0 0 0 -2.5-2.5z"/>
+                </svg>
+              </NavLink>
+            </div>
+          </div>
+          <div className={classes.friends__content}>
+            <div className={classes.friends__info}>
+              <NavLink to="#">
+                <img src={card_image} alt="img_card"/>
+              </NavLink>
+              <div className={classes.text}>
+                <h3><NavLink to="#">Jose Marroquin</NavLink></h3>
+                <span>10 Mutual Friends</span>
+              </div>
+            </div>
+            <ul className={classes.statistics}>
+              <li>
+                <NavLink to="#">
+                  <span className={classes.item_number}>862</span>
+                  <span className={classes.item_text}>Likes</span>
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="#">
+                  <span className={classes.item_number}>91</span>
+                  <span className={classes.item_text}>Following</span>
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="#">
+                  <span className={classes.item_number}>514</span>
+                  <span className={classes.item_text}>Followers</span>
+                </NavLink>
+              </li>
+            </ul>
+            <div className={classes.button_group}>
+              <div className={classes.add_friend_btn}>
+                <button type="submit">Add Friend</button>
+              </div>
+              <div className={classes.send_message_btn}>
+                <button type="submit">Send Message</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={classes.card}>
+          <div className={classes.friends__image}>
+            <NavLink to="#">
+              <img src={card_image_bg} alt="img_bg"/>
+            </NavLink>
+            <div className={classes.icon}>
+              <NavLink className={classes.link} to="#">
+                <svg height="20" viewBox="0 0 24 24" width="20"
+                     xmlns="http://www.w3.org/2000/svg"
+                     fill='grey'>
+                  <path
+                    d="m7.5 13a4.5 4.5 0 1 1 4.5-4.5 4.505 4.505 0 0 1 -4.5 4.5zm0-7a2.5 2.5 0 1 0 2.5 2.5 2.5 2.5 0 0 0 -2.5-2.5zm7.5 17v-.5a7.5 7.5 0 0 0 -15 0v.5a1 1 0 0 0 2 0v-.5a5.5 5.5 0 0 1 11 0v.5a1 1 0 0 0 2 0zm9-5a7 7 0 0 0 -11.667-5.217 1 1 0 1 0 1.334 1.49 5 5 0 0 1 8.333 3.727 1 1 0 0 0 2 0zm-6.5-9a4.5 4.5 0 1 1 4.5-4.5 4.505 4.505 0 0 1 -4.5 4.5zm0-7a2.5 2.5 0 1 0 2.5 2.5 2.5 2.5 0 0 0 -2.5-2.5z"/>
+                </svg>
+              </NavLink>
+            </div>
+          </div>
+          <div className={classes.friends__content}>
+            <div className={classes.friends__info}>
+              <NavLink to="#">
+                <img src={card_image} alt="img_card"/>
+              </NavLink>
+              <div className={classes.text}>
+                <h3><NavLink to="#">Jose Marroquin</NavLink></h3>
+                <span>10 Mutual Friends</span>
+              </div>
+            </div>
+            <ul className={classes.statistics}>
+              <li>
+                <NavLink to="#">
+                  <span className={classes.item_number}>862</span>
+                  <span className={classes.item_text}>Likes</span>
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="#">
+                  <span className={classes.item_number}>91</span>
+                  <span className={classes.item_text}>Following</span>
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="#">
+                  <span className={classes.item_number}>514</span>
+                  <span className={classes.item_text}>Followers</span>
+                </NavLink>
+              </li>
+            </ul>
+            <div className={classes.button_group}>
+              <div className={classes.add_friend_btn}>
+                <button type="submit">Add Friend</button>
+              </div>
+              <div className={classes.send_message_btn}>
+                <button type="submit">Send Message</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={classes.card}>
+          <div className={classes.friends__image}>
+            <NavLink to="#">
+              <img src={card_image_bg} alt="img_bg"/>
+            </NavLink>
+            <div className={classes.icon}>
+              <NavLink className={classes.link} to="#">
+                <svg height="20" viewBox="0 0 24 24" width="20"
+                     xmlns="http://www.w3.org/2000/svg"
+                     fill='grey'>
+                  <path
+                    d="m7.5 13a4.5 4.5 0 1 1 4.5-4.5 4.505 4.505 0 0 1 -4.5 4.5zm0-7a2.5 2.5 0 1 0 2.5 2.5 2.5 2.5 0 0 0 -2.5-2.5zm7.5 17v-.5a7.5 7.5 0 0 0 -15 0v.5a1 1 0 0 0 2 0v-.5a5.5 5.5 0 0 1 11 0v.5a1 1 0 0 0 2 0zm9-5a7 7 0 0 0 -11.667-5.217 1 1 0 1 0 1.334 1.49 5 5 0 0 1 8.333 3.727 1 1 0 0 0 2 0zm-6.5-9a4.5 4.5 0 1 1 4.5-4.5 4.505 4.505 0 0 1 -4.5 4.5zm0-7a2.5 2.5 0 1 0 2.5 2.5 2.5 2.5 0 0 0 -2.5-2.5z"/>
+                </svg>
+              </NavLink>
+            </div>
+          </div>
+          <div className={classes.friends__content}>
+            <div className={classes.friends__info}>
+              <NavLink to="#">
+                <img src={card_image} alt="img_card"/>
+              </NavLink>
+              <div className={classes.text}>
+                <h3><NavLink to="#">Jose Marroquin</NavLink></h3>
+                <span>10 Mutual Friends</span>
+              </div>
+            </div>
+            <ul className={classes.statistics}>
+              <li>
+                <NavLink to="#">
+                  <span className={classes.item_number}>862</span>
+                  <span className={classes.item_text}>Likes</span>
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="#">
+                  <span className={classes.item_number}>91</span>
+                  <span className={classes.item_text}>Following</span>
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="#">
+                  <span className={classes.item_number}>514</span>
+                  <span className={classes.item_text}>Followers</span>
+                </NavLink>
+              </li>
+            </ul>
+            <div className={classes.button_group}>
+              <div className={classes.add_friend_btn}>
+                <button type="submit">Add Friend</button>
+              </div>
+              <div className={classes.send_message_btn}>
+                <button type="submit">Send Message</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={classes.card}>
+          <div className={classes.friends__image}>
+            <NavLink to="#">
+              <img src={card_image_bg} alt="img_bg"/>
+            </NavLink>
+            <div className={classes.icon}>
+              <NavLink className={classes.link} to="#">
+                <svg height="20" viewBox="0 0 24 24" width="20"
+                     xmlns="http://www.w3.org/2000/svg"
+                     fill='grey'>
+                  <path
+                    d="m7.5 13a4.5 4.5 0 1 1 4.5-4.5 4.505 4.505 0 0 1 -4.5 4.5zm0-7a2.5 2.5 0 1 0 2.5 2.5 2.5 2.5 0 0 0 -2.5-2.5zm7.5 17v-.5a7.5 7.5 0 0 0 -15 0v.5a1 1 0 0 0 2 0v-.5a5.5 5.5 0 0 1 11 0v.5a1 1 0 0 0 2 0zm9-5a7 7 0 0 0 -11.667-5.217 1 1 0 1 0 1.334 1.49 5 5 0 0 1 8.333 3.727 1 1 0 0 0 2 0zm-6.5-9a4.5 4.5 0 1 1 4.5-4.5 4.505 4.505 0 0 1 -4.5 4.5zm0-7a2.5 2.5 0 1 0 2.5 2.5 2.5 2.5 0 0 0 -2.5-2.5z"/>
+                </svg>
+              </NavLink>
+            </div>
+          </div>
+          <div className={classes.friends__content}>
+            <div className={classes.friends__info}>
+              <NavLink to="#">
+                <img src={card_image} alt="img_card"/>
+              </NavLink>
+              <div className={classes.text}>
+                <h3><NavLink to="#">Jose Marroquin</NavLink></h3>
+                <span>10 Mutual Friends</span>
+              </div>
+            </div>
+            <ul className={classes.statistics}>
+              <li>
+                <NavLink to="#">
+                  <span className={classes.item_number}>862</span>
+                  <span className={classes.item_text}>Likes</span>
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="#">
+                  <span className={classes.item_number}>91</span>
+                  <span className={classes.item_text}>Following</span>
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="#">
+                  <span className={classes.item_number}>514</span>
+                  <span className={classes.item_text}>Followers</span>
+                </NavLink>
+              </li>
+            </ul>
+            <div className={classes.button_group}>
+              <div className={classes.add_friend_btn}>
+                <button type="submit">Add Friend</button>
+              </div>
+              <div className={classes.send_message_btn}>
+                <button type="submit">Send Message</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={classes.card}>
+          <div className={classes.friends__image}>
+            <NavLink to="#">
+              <img src={card_image_bg} alt="img_bg"/>
+            </NavLink>
+            <div className={classes.icon}>
+              <NavLink className={classes.link} to="#">
+                <svg height="20" viewBox="0 0 24 24" width="20"
+                     xmlns="http://www.w3.org/2000/svg"
+                     fill='grey'>
+                  <path
+                    d="m7.5 13a4.5 4.5 0 1 1 4.5-4.5 4.505 4.505 0 0 1 -4.5 4.5zm0-7a2.5 2.5 0 1 0 2.5 2.5 2.5 2.5 0 0 0 -2.5-2.5zm7.5 17v-.5a7.5 7.5 0 0 0 -15 0v.5a1 1 0 0 0 2 0v-.5a5.5 5.5 0 0 1 11 0v.5a1 1 0 0 0 2 0zm9-5a7 7 0 0 0 -11.667-5.217 1 1 0 1 0 1.334 1.49 5 5 0 0 1 8.333 3.727 1 1 0 0 0 2 0zm-6.5-9a4.5 4.5 0 1 1 4.5-4.5 4.505 4.505 0 0 1 -4.5 4.5zm0-7a2.5 2.5 0 1 0 2.5 2.5 2.5 2.5 0 0 0 -2.5-2.5z"/>
+                </svg>
+              </NavLink>
+            </div>
+          </div>
+          <div className={classes.friends__content}>
+            <div className={classes.friends__info}>
+              <NavLink to="#">
+                <img src={card_image} alt="img_card"/>
+              </NavLink>
+              <div className={classes.text}>
+                <h3><NavLink to="#">Jose Marroquin</NavLink></h3>
+                <span>10 Mutual Friends</span>
+              </div>
+            </div>
+            <ul className={classes.statistics}>
+              <li>
+                <NavLink to="#">
+                  <span className={classes.item_number}>862</span>
+                  <span className={classes.item_text}>Likes</span>
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="#">
+                  <span className={classes.item_number}>91</span>
+                  <span className={classes.item_text}>Following</span>
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="#">
+                  <span className={classes.item_number}>514</span>
+                  <span className={classes.item_text}>Followers</span>
+                </NavLink>
+              </li>
+            </ul>
+            <div className={classes.button_group}>
+              <div className={classes.add_friend_btn}>
+                <button type="submit">Add Friend</button>
+              </div>
+              <div className={classes.send_message_btn}>
                 <button type="submit">Send Message</button>
               </div>
             </div>
