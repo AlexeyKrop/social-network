0bfbd674f80051f9a840ef57d1f5dd28a24151cb
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-05-22 17:01:15 +0300

    add server request

--- src/components/Friends/Friends.tsx
+++ src/components/Friends/Friends.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import classes from './Friends.module.css'
 import CardFriend from './CardFriend/CardFriend';
 import {UserStateType} from "../../Redux/friendsPageReducer";
-import {v1} from "uuid";
+import axios from "axios";
 
 // type cardFriendType = {
 //   cardAvatar: string,
@@ -22,56 +22,61 @@ export type cardFriendsPropsType = {
 }
 const Friends = (props: cardFriendsPropsType) => {
   if (props.cardFriends.length === 0) {
-    props.setUSer([
-      {
-        cardAvatar: 'https://templates.envytheme.com/zust/default/assets/images/friends/friends-1.jpg',
-        cardBg: 'https://templates.envytheme.com/zust/default/assets/images/friends/friends-bg-1.jpg',
-        user_name: 'Jose Marroquin',
-        id: v1(),
-        friendStatus: true,
-        sendMessageStatus: false
-      },
-      {
-        cardAvatar: 'https://templates.envytheme.com/zust/default/assets/images/friends/friends-2.jpg',
-        cardBg: 'https://templates.envytheme.com/zust/default/assets/images/friends/friends-bg-2.jpg',
-        user_name: 'Myrtle Lewis',
-        id: v1(),
-        friendStatus: true,
-        sendMessageStatus: false
-      },
-      {
-        cardAvatar: 'https://templates.envytheme.com/zust/default/assets/images/friends/friends-3.jpg',
-        cardBg: 'https://templates.envytheme.com/zust/default/assets/images/friends/friends-bg-3.jpg',
-        user_name: 'Howard Tam',
-        id: v1(),
-        friendStatus: false,
-        sendMessageStatus: false
-      },
-      {
-        cardAvatar: 'https://templates.envytheme.com/zust/default/assets/images/friends/friends-4.jpg',
-        cardBg: 'https://templates.envytheme.com/zust/default/assets/images/friends/friends-bg-4.jpg',
-        user_name: 'Kimberly Blum',
-        id: v1(),
-        friendStatus: true,
-        sendMessageStatus: false
-      },
-      {
-        cardAvatar: 'https://templates.envytheme.com/zust/default/assets/images/friends/friends-5.jpg',
-        cardBg: 'https://templates.envytheme.com/zust/default/assets/images/friends/friends-bg-5.jpg',
-        user_name: 'Mary Mercado',
-        id: v1(),
-        friendStatus: false,
-        sendMessageStatus: false
-      },
-      {
-        cardAvatar: 'https://templates.envytheme.com/zust/default/assets/images/friends/friends-6.jpg',
-        cardBg: 'https://templates.envytheme.com/zust/default/assets/images/friends/friends-bg-6.jpg',
-        user_name: 'Robert Ward',
-        id: v1(),
-        friendStatus: false,
-        sendMessageStatus: false
-      }
-    ])
+    axios.get('https://social-network.samuraijs.com/api/1.0/users')
+      .then((response) => {
+        // обработка успешного запроса
+        props.setUSer(response.data.items)
+      })
+    // props.setUSer([
+    //   {
+    //     cardAvatar: 'https://templates.envytheme.com/zust/default/assets/images/friends/friends-1.jpg',
+    //     cardBg: 'https://templates.envytheme.com/zust/default/assets/images/friends/friends-bg-1.jpg',
+    //     user_name: 'Jose Marroquin',
+    //     id: v1(),
+    //     friendStatus: true,
+    //     sendMessageStatus: false
+    //   },
+    //   {
+    //     cardAvatar: 'https://templates.envytheme.com/zust/default/assets/images/friends/friends-2.jpg',
+    //     cardBg: 'https://templates.envytheme.com/zust/default/assets/images/friends/friends-bg-2.jpg',
+    //     user_name: 'Myrtle Lewis',
+    //     id: v1(),
+    //     friendStatus: true,
+    //     sendMessageStatus: false
+    //   },
+    //   {
+    //     cardAvatar: 'https://templates.envytheme.com/zust/default/assets/images/friends/friends-3.jpg',
+    //     cardBg: 'https://templates.envytheme.com/zust/default/assets/images/friends/friends-bg-3.jpg',
+    //     user_name: 'Howard Tam',
+    //     id: v1(),
+    //     friendStatus: false,
+    //     sendMessageStatus: false
+    //   },
+    //   {
+    //     cardAvatar: 'https://templates.envytheme.com/zust/default/assets/images/friends/friends-4.jpg',
+    //     cardBg: 'https://templates.envytheme.com/zust/default/assets/images/friends/friends-bg-4.jpg',
+    //     user_name: 'Kimberly Blum',
+    //     id: v1(),
+    //     friendStatus: true,
+    //     sendMessageStatus: false
+    //   },
+    //   {
+    //     cardAvatar: 'https://templates.envytheme.com/zust/default/assets/images/friends/friends-5.jpg',
+    //     cardBg: 'https://templates.envytheme.com/zust/default/assets/images/friends/friends-bg-5.jpg',
+    //     user_name: 'Mary Mercado',
+    //     id: v1(),
+    //     friendStatus: false,
+    //     sendMessageStatus: false
+    //   },
+    //   {
+    //     cardAvatar: 'https://templates.envytheme.com/zust/default/assets/images/friends/friends-6.jpg',
+    //     cardBg: 'https://templates.envytheme.com/zust/default/assets/images/friends/friends-bg-6.jpg',
+    //     user_name: 'Robert Ward',
+    //     id: v1(),
+    //     friendStatus: false,
+    //     sendMessageStatus: false
+    //   }
+    // ])
   }
   let Cards = props.cardFriends.map(item => <CardFriend key={item.id} addFriend={props.addFriend}
                                                         delFriend={props.delFriend}
