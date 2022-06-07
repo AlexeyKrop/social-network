2eb8059e50808310d6189bb6d930c806ab73b05d
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-06-07 09:31:57 +0300

    add authReducer in store

--- src/Redux/redux-store.tsx
+++ src/Redux/redux-store.tsx
@@ -3,11 +3,13 @@ import {combineReducers, createStore} from "redux";
 import addMessagesInMessagePageReducer from "./addMessagesInMessagePageReducer";
 import friendsPageReducer from "./friendsPageReducer";
 import {profilePageReducer} from "./profilePageReducer";
+import {authReducer} from "./authorization-reducer";
 
 const rootReducer = combineReducers({
   ProfilePage: profilePageReducer,
   MessagePage: addMessagesInMessagePageReducer,
   FriendsPage: friendsPageReducer,
+  Authorization: authReducer,
 })
 export type AppStateType = ReturnType<typeof rootReducer>
 let store = createStore(rootReducer);
