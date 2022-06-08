f6b4c4d8d24020763cf6f2175bd7d075a861e535
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-06-08 15:44:18 +0300

    add baseURL in instance

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,21 +2,22 @@ import axios from "axios";
 
 const instance = axios.create({
   withCredentials: true,
+  baseURL: `https://social-network.samuraijs.com/api/1.0/`,
   headers: {
     'API-KEY': 'a3194ef5-a5fe-47c5-b463-8be5589a0929'
   }
 })
 
-const baseURL = `https://social-network.samuraijs.com/api/1.0/`
+// const baseURL = `https://social-network.samuraijs.com/api/1.0/`
 export const userAPI = {
   getUser(currentPageNumber: number, pageSize: number) {
-    return instance.get(baseURL + `users?page=${currentPageNumber}&count=${pageSize}`).then(response => response.data)
+    return instance.get(`users?page=${currentPageNumber}&count=${pageSize}`).then(response => response.data)
   },
   deleteUser(id: number) {
-    return instance.delete(baseURL + `follow/${id}`).then(response => response.data)
+    return instance.delete(`follow/${id}`).then(response => response.data)
   },
   addUser(id: number) {
-    return instance.post(baseURL + `follow/${id}`, {}).then(response => response.data)
+    return instance.post(`follow/${id}`, {}).then(response => response.data)
   }
 }
 // export const getUser = (currentPageNumber: number, pageSize: number) => {
