9afcf62077ad26d15e9dd96b34cc3417eb63c759
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-07-04 10:11:02 +0300

    add Redirect

--- src/components/Login/LoginForm.tsx
+++ src/components/Login/LoginForm.tsx
@@ -6,7 +6,7 @@ const LoginForm = (props: InjectedFormProps) => {
   return (
     <form onSubmit={props.handleSubmit}>
       <Field name="email" placeholder={'login'} component="input" type="text"/>
-      <Field name="password" placeholder={'password'} component="input" type="text"/>
+      <Field name="password" placeholder={'password'} component="input" type="password"/>
       <Field name="checkbox" component="input" type="checkbox"/> remember me
       <button>Enter</button></form>
   )
@@ -14,5 +14,4 @@ const LoginForm = (props: InjectedFormProps) => {
 };
 
 export const LoginReduxForm =
-  reduxForm({form: 'login'})
-  (LoginForm);
+  reduxForm({form: 'login'})(LoginForm);
