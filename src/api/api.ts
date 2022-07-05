86b147adb1a5c6c1c157d63ae2e9fdcc6e473d05
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-07-05 22:10:08 +0300

    add ResponseType

--- src/api/api.ts
+++ src/api/api.ts
@@ -9,29 +9,37 @@ const instance = axios.create({
   }
 })
 
-type ResponseType = {
+type ResponseGetUserType = {
   items: Array<UserStateType>
   totalCount: number
   error: null
+  resultCode: number
+  messages: Array<string>,
 }
+type ResponseType<D = {}> = {
+  resultCode: number
+  messages: Array<string>,
+  data: D
+}
+
 export const userAPI = {
   getUser(currentPageNumber: number, pageSize: number) {
-    return instance.get<ResponseType>(`users?page=${currentPageNumber}&count=${pageSize}`).then(response => response.data)
+    return instance.get<ResponseGetUserType>(`users?page=${currentPageNumber}&count=${pageSize}`).then(response => response.data)
   },
   deleteUser(id: number) {
-    return instance.delete(`follow/${id}`).then(response => response.data)
+    return instance.delete<ResponseType>(`follow/${id}`).then(response => response.data)
   },
   addUser(id: number) {
-    return instance.post(`follow/${id}`, {}).then(response => response.data)
+    return instance.post<ResponseType>(`follow/${id}`, {}).then(response => response.data)
   },
   setProfileUser(userID: number) {
-    return instance.get(`profile/${userID}`).then(response => response.data)
+    return instance.get<UserStateType>(`profile/${userID}`).then(response => response.data)
   },
 }
 
 export const authMe = {
   me() {
-    return instance.get('auth/me')
+    return instance.get<ResponseType<{ id: number, email: string, login: string }>>('auth/me')
   },
   login(email: string, password: string, rememberMe: boolean) {
     return instance.post(`/auth/login`, {email, password, rememberMe})
