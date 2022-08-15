eb0044aecdc65acffafadc745fc00c22bb21c34c
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-08-15 08:34:02 +0300

    add Redirect to profile

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,14 @@ import './App.css';
 import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
 import ProfileContainer from "./components/Profile/ProfileContainer";
-import {BrowserRouter, Route} from "react-router-dom";
+import { BrowserRouter, Redirect, Route } from "react-router-dom";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
 import Dropdown from "./components/Dropdown/Dropdown";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import FriendsContainer from "./components/Friends/FriendsContainer";
-import {Login} from "./components/Login/Login";
+import { Login } from "./components/Login/Login";
 import 'antd/dist/antd.css'
 
 
@@ -19,23 +19,24 @@ function App() {
     <BrowserRouter>
       <div className="App">
         <div className="container">
-          <Header/>
+          <Header />
           <div className="wrapper">
-            <Sidebar/>
+            <Sidebar />
             <main className="content">
-              <Route path="/profile/:userId?" render={() => <ProfileContainer/>}/>
+              <Route path="/" render={() => <Redirect to="/profile" />} />
+              <Route path="/profile/:userId?" render={() => <ProfileContainer />} />
               <Route path="/friends"
-                     render={() => <FriendsContainer/>}/>
+                render={() => <FriendsContainer />} />
               <Route path="/dialogs"
-                     render={() => <DialogsContainer/>}/>
-              <Route path="/news" render={() => <News/>}/>
-              <Route path="/music" render={() => <Music/>}/>
-              <Route path="/settings" render={() => <Settings/>}/>
-              <Route path="/login" render={() => <Login/>}/>
+                render={() => <DialogsContainer />} />
+              <Route path="/news" render={() => <News />} />
+              <Route path="/music" render={() => <Music />} />
+              <Route path="/settings" render={() => <Settings />} />
+              <Route path="/login" render={() => <Login />} />
             </main>
           </div>
         </div>
-        <Dropdown/>
+        <Dropdown />
       </div>
     </BrowserRouter>
   );
