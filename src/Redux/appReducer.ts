0ba7de703473a9222c54da619d7d036e13941308
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-08-19 17:28:16 +0300

    add setAppInitializedAC

--- src/Redux/appReducer.ts
+++ src/Redux/appReducer.ts
@@ -2,7 +2,8 @@ export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
 
 const initialState = {
   status: 'loading' as RequestStatusType,
-  error: null as null | string
+  error: null as null | string,
+  initialized: false,
 }
 
 type InitialStateType = typeof initialState
@@ -24,8 +25,10 @@ export const appReducer = (state: InitialStateType = initialState, action: AppRe
 //ACTIONS CREATOR
 export const setAppStatusAC = (status: RequestStatusType) => ({type: 'APP/SET-STATUS', status} as const)
 export const setAppErrorAC = (error: string | null) => ({type: 'APP/SET-ERROR', error} as const)
+export const setAppInitializedAC = (initialized: boolean) => ({type: 'APP/SET-INITIALIZED', initialized} as const)
 //TYPES
 export type ActionsAppType = SetAppStatusAT | SetAppErrorAT | AppReducerType
 type SetAppStatusAT = ReturnType<typeof setAppStatusAC>
 type SetAppErrorAT = ReturnType<typeof setAppErrorAC>
+type SetAppInitializedAT = ReturnType<typeof setAppInitializedAC>
 type AppReducerType = SetAppStatusAT | SetAppErrorAT
